9b06a9bca04c264591c49f6ff95bf898e9eaa6c2
ZhangYu <939476975@qq.com>
2020-12-26 22:21:34 +0800

    当前只有函数初始形态，后续完善

--- getPrecodeMat.m
+++ getPrecodeMat.m
@@ -1,5 +1,5 @@
 %% 计算预编码矩阵
-function precode_mat = getPrecodeMat(scene,g_AP_PU,g_AP_SUs,decode_mat,weight_mat,precode_mat)   
+function precode_mat = getPrecodeMat()   
     %% 引入函数集
     func = normalFuncSet;
     %% 计算相关参数
